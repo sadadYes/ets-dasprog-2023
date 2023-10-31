c3e60eb196240824b2e4cf08c8a2456f297890fa
Sadad <86421613+sadadYes@users.noreply.github.com>
2023-11-01 00:23:01 +0700

    Create Soal_No_2.c

---
+++ Soal_No_2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+// Deklarasi fungsi faktorial
+int faktorial(int n) {
+    if (n == 0)
+        return 1;
+    else
+        return n * faktorial(n - 1);
+}
+
+int main() {
+    int n, r;
+    int faktorial_n, faktorial_r, faktorial_n_minus_r;
+    int kombinasi;
+
+    // Meminta pengguna memasukkan nilai n dan r
+    printf("Masukkan nilai n: \n");
+    scanf("%d", &n);
+    printf("Masukkan nilai r: \n");
+    scanf("%d", &r);
+
+    // Menghitung faktorial n, r, dan (n - r)
+    faktorial_n = faktorial(n);
+    faktorial_r = faktorial(r);
+    faktorial_n_minus_r = faktorial(n - r);
+
+    // Menghitung kombinasi
+    kombinasi = faktorial_n / (faktorial_r * faktorial_n_minus_r);
+
+    // Menampilkan nilai kombinasi
+    printf("Kombinasi = %d\n", kombinasi);
+
+    return 0;
